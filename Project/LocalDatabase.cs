aecd45d44ee1f1b1ed2cf8d3b64550d8b1fba018
MuflihAzhar07 <uli.azhar07@gmail.com>
2026-06-19 09:42:42 +0700

    Add files via upload

---
+++ Project/LocalDatabase.cs
@@ -0,0 +1,40 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Maui.Storage;
+
+namespace VitalGuard
+{
+    public class LocalDatabase
+    {
+        private SQLiteAsyncConnection _database;
+
+        async Task Init()
+        {
+            if (_database is not null)
+                return;
+
+            // Membuat file database di penyimpanan internal perangkat
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "VitalGuard.db3");
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            // Membuat tabel berdasarkan struktur AnomalyLog
+            await _database.CreateTableAsync<AnomalyLog>();
+        }
+
+        public async Task<List<AnomalyLog>> GetLogsAsync()
+        {
+            await Init();
+            // Mengambil semua data dan mengurutkannya dari yang paling baru
+            return await _database.Table<AnomalyLog>().OrderByDescending(x => x.Id).ToListAsync();
+        }
+
+        public async Task<int> SaveLogAsync(AnomalyLog log)
+        {
+            await Init();
+            // Menyimpan data anomali baru
+            return await _database.InsertAsync(log);
+        }
+    }
+}
